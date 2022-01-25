48cd75e23a876c1853b80d14525718b0eec7cc10
GyeonghoPark <kayheypark@gmail.com>
2022-01-25 15:56:01 +0900

    로그인에 css 컴포넌트화

--- src/component/login/index.jsx
+++ src/component/login/index.jsx
@@ -2,6 +2,21 @@ import styles from  './index.module.css';
 
 export default function Login() {
   return (
-    <div className={styles.title}>{styles.title}로그인</div>
+      <div>
+        <form name="JoinForm" id="JoinForm" method="POST">
+            <fieldset>
+                <legend>로그인</legend>
+                <div className={styles.form_box}>
+                    <label htmlFor="UserID">아이디</label>
+                    <input type="text" name="UserID" id="UserID" value="" placeholder="아이디를 입력해주세요" required />
+                </div>
+                <div className={styles.form_box}>
+                    <label htmlFor="UserPassword">비밀번호</label>
+                    <input type="password" name="UserPassword" id="UserPassword" value="" placeholder="비밀번호를 입력해주세요" required />
+                </div>
+            </fieldset>
+            <button className={styles.button} type="submit">로그인</button>
+        </form>
+      </div>
   );
 };
--- src/page/main/index.jsx
+++ src/page/main/index.jsx
@@ -1,1 +1,10 @@
-﻿
+import Login from '../../component/login';
+
+export default function Main() {
+  return (
+    <div className="Main">
+        메인
+        <Login />
+    </div>
+  );
+};
